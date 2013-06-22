c20fc98360a9371fa5ef008e421e5f7bd5a49fe1
henrietta <piotr.maslanka@henrietta.com.pl>
2013-06-22 23:21:48 +0200

    GSON template objects moved to shared

---
+++ shared/com/sparkfighters/shared/loader/jsonobjs/hero/HeroData.java
@@ -0,0 +1,11 @@
+package com.sparkfighters.shared.loader.jsonobjs.hero;
+
+import java.util.ArrayList;
+
+import com.sparkfighters.shared.loader.jsonobjs.hero.AnimationData;
+
+public class HeroData 
+{
+	public String HeroName;
+	public ArrayList<AnimationData> Animations=new ArrayList<AnimationData>();
+}
---
+++ shared/com/sparkfighters/shared/loader/jsonobjs/hero/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes related to Hero-type JSON packages
+ * @author Henrietta
+ *
+ */
+package com.sparkfighters.shared.loader.jsonobjs.hero;
