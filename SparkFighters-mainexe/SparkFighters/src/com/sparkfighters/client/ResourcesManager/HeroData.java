4aa5648cbb9a2e6fae8f7f94c2cf4a3b73dbc9b9
Kamil Iwiński <kamil3453@yahoo.com>
2013-07-15 23:25:57 +0200

    Added heroes and guns. Modified HeroData, AnimationData class

--- SparkFighters-mainexe/SparkFighters/src/com/sparkfighters/client/ResourcesManager/HeroData.java
+++ SparkFighters-mainexe/SparkFighters/src/com/sparkfighters/client/ResourcesManager/HeroData.java
@@ -2,10 +2,21 @@ package com.sparkfighters.client.ResourcesManager;
 
 import java.util.ArrayList;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+
 
 
 public class HeroData 
 {
 	public String HeroName;
 	public ArrayList<AnimationData> Animations=new ArrayList<AnimationData>();
+	public Texture texture;
+	
+	public void loadTexture(String filePath)
+	{
+		texture=new Texture(filePath);
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+	}
+	
 }
