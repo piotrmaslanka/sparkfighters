cc0b53985b0f9fd1118a1f3dce8d33d7a2106c7f
Kamil Iwiński <kamil3453@yahoo.com>
2013-08-16 16:41:43 +0200

    netowrk added

--- client_game/src/com/sparkfighters/client/game/singletons/Network.java
+++ client_game/src/com/sparkfighters/client/game/singletons/Network.java
@@ -1,6 +1,7 @@
 package com.sparkfighters.client.game.singletons;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
@@ -13,11 +14,14 @@ import pl.com.henrietta.lnx2.Connection;
 import pl.com.henrietta.lnx2.Packet;
 import pl.com.henrietta.lnx2.RetransmissionMode;
 import pl.com.henrietta.lnx2.exceptions.NothingToRead;
+import pl.com.henrietta.lnx2.exceptions.NothingToSend;
 import pl.com.henrietta.lnx2.exceptions.PacketMalformedError;
 
 public enum Network
 {
 	INSTANCE;
+	private String login;
+	private String password;
 	
 	private String ip;
 	private int port;
@@ -26,10 +30,26 @@ public enum Network
 	private DatagramChannel channel;
 	private Connection connection;
 	
-	public void Init(String ip, int port)
+	public void Connect(String login, String password, String ip, int port) throws IOException, NothingToSend, PacketMalformedError, NothingToRead
 	{
 		this.ip=ip;
 		this.port=port;
+		this.login=login;
+		this.password=password;
+		
+		//create real connection
+		channel = DatagramChannel.open();
+		channel.socket().bind(new InetSocketAddress(ip, port));
+		setBlocking(true);
+				
+		//setting channels
+		Vector<Channel> chanells=new Vector<Channel>();	
+		Channel c0=new Channel((byte)0, RetransmissionMode.RTM_AUTO_ORDERED, 10, 60);
+		chanells.add(c0);
+				
+		connection=new Connection(chanells, 15f);
+		
+		AuthorizeConnection();
 	}
 	
 	public void setBlocking(boolean blocking) throws IOException
@@ -38,21 +58,7 @@ public enum Network
 		channel.configureBlocking(this.blocking);
 	}
 	
-	public void Connect() throws IOException
-	{
-		//create real connection
-		channel = DatagramChannel.open();
-		channel.socket().bind(new InetSocketAddress(ip, port));
-		setBlocking(true);
-		
-		//setting channels
-		Vector<Channel> chanells=new Vector<Channel>();	
-		Channel c0=new Channel((byte)0, RetransmissionMode.RTM_AUTO_ORDERED, 10, 60);
-		chanells.add(c0);
-		
-		connection=new Connection(chanells, 15f);
-			
-	}
+	
 	
 	public void Recive() throws IOException, PacketMalformedError, NothingToRead
 	{
@@ -62,15 +68,39 @@ public enum Network
 		
 		if(this.connection.has_new_data==true)
 		{
-			connection.getChannel(0).read();
+			 for (Channel c : connection.getChannels()) 
+			 {
+				 String msg = new String(connection.getChannel(c.channel_id).read(), "UTF-8");
+				 DoCommand(c.channel_id, msg);
+			 }
 		}
 	}
 	
-	public void Send()
+	public void Send(byte channel, String text) throws NothingToSend, IOException
 	{
-		//this.connection.getChannel(0).write(byte[])
+		this.connection.getChannel(channel).write(text.getBytes("UTF-8"));	
+		Packet p=this.connection.on_sendable();	
+		ByteBuffer buf=ByteBuffer.wrap(p.data);	
+		this.channel.write(buf);
 	}
 	
 
+	private void DoCommand(byte channel, String msg)
+	{
+		if(channel==0)
+		{
+			
+		}
+	}
+	
+	private void AuthorizeConnection() throws NothingToSend, IOException, PacketMalformedError, NothingToRead
+	{
+		Send((byte) 0, login);
+		Recive(); //for nonce
+		Recive(); //for resposne OK/failed
+	}
+	
+	
+	
 	
 }
