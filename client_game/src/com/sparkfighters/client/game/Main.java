168f5bfb1820c9f7161ae5612b1b146873fa34bc
Kamil Iwiński <kamil3453@yahoo.com>
2013-08-21 01:15:31 +0200

    add maven config

--- client_game/src/com/sparkfighters/client/game/Main.java
+++ client_game/src/com/sparkfighters/client/game/Main.java
@@ -3,10 +3,24 @@ package com.sparkfighters.client.game;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+import net.java.truevfs.access.TFile;
+import net.java.truevfs.access.TFileOutputStream;
+import net.java.truevfs.access.TFileReader;
+import net.java.truevfs.access.TFileWriter;
+
+import pl.com.henrietta.lnx2.exceptions.NothingToRead;
+import pl.com.henrietta.lnx2.exceptions.NothingToSend;
+import pl.com.henrietta.lnx2.exceptions.PacketMalformedError;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sparkfighters.client.game.singletons.Logger;
+import com.sparkfighters.client.game.singletons.Network;
+import com.sparkfighters.client.game.ultis.VFS;
 import com.sparkfighters.client.monitor.Monitor;
 
 /**
@@ -38,14 +52,14 @@ public class Main {
 	   return content;
 	}
 	
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		Logger.INSTANCE.write("SparkFightersGame.jar", Logger.LogType.INFO);
 		
 		Monitor m=new Monitor();
 		String jarName=new java.io.File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
 		if(m.launch(jarName, Main.class,"Spark Fighters Game")==true)
-		{
+		{		
 			String s=readFile("data/cfg.cfg");
 			String[] s2=s.split(" ");
 			
@@ -67,7 +81,14 @@ public class Main {
 			cfg.foregroundFPS=fps;
 			
 			//cfg.addIcon("logo.png", FileType.Internal);
-			
+			try 
+			{
+				Network.INSTANCE.Init(args[0], args[1], args[2], args[3]);
+			}
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
 			new LwjglApplication(new SparkFightersGame(), cfg);
 		}
 		else
--- client_game/src/com/sparkfighters/client/game/singletons/Network.java
+++ client_game/src/com/sparkfighters/client/game/singletons/Network.java
@@ -6,6 +6,9 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Vector;
 
@@ -30,26 +33,30 @@ public enum Network
 	private DatagramChannel channel;
 	private Connection connection;
 	
-	public void Connect(String login, String password, String ip, int port) throws IOException, NothingToSend, PacketMalformedError, NothingToRead
+	public void Init(String login, String password, String ip, String port)
 	{
 		this.ip=ip;
-		this.port=port;
+		this.port=Integer.valueOf(port);
 		this.login=login;
 		this.password=password;
-		
+	}
+	
+	public void Connect() throws IOException, NothingToSend, PacketMalformedError, NothingToRead, NoSuchAlgorithmException
+	{
 		//create real connection
 		channel = DatagramChannel.open();
-		channel.socket().bind(new InetSocketAddress(ip, port));
+		channel.socket().bind(new InetSocketAddress(0));
 		setBlocking(true);
-				
+						
 		//setting channels
 		Vector<Channel> chanells=new Vector<Channel>();	
 		Channel c0=new Channel((byte)0, RetransmissionMode.RTM_AUTO_ORDERED, 10, 60);
 		chanells.add(c0);
-				
+						
 		connection=new Connection(chanells, 15f);
 		
 		AuthorizeConnection();
+	
 	}
 	
 	public void setBlocking(boolean blocking) throws IOException
@@ -60,40 +67,64 @@ public enum Network
 	
 	
 	
-	public void Recive() throws IOException, PacketMalformedError, NothingToRead
+	public void Recive() throws IOException, PacketMalformedError, NothingToRead, NoSuchAlgorithmException, NothingToSend
 	{
 		ByteBuffer recvbuf = ByteBuffer.allocate(2048);
-		this.channel.read(recvbuf);
-		this.connection.on_received(Packet.from_bytes(recvbuf.array()));
+		this.channel.receive(recvbuf);
 		
+		this.connection.on_received(Packet.from_bytes(recvbuf.array()));
+
 		if(this.connection.has_new_data==true)
 		{
-			 for (Channel c : connection.getChannels()) 
-			 {
-				 String msg = new String(connection.getChannel(c.channel_id).read(), "UTF-8");
-				 DoCommand(c.channel_id, msg);
-			 }
+			for (Channel c : connection.getChannels()) 
+			{
+				String msg = new String(connection.getChannel(c.channel_id).read(), "UTF-8");
+				DoCommand(c.channel_id, msg);
+			}
 		}
+
+			
 	}
 	
 	public void Send(byte channel, String text) throws NothingToSend, IOException
 	{
 		this.connection.getChannel(channel).write(text.getBytes("UTF-8"));	
 		Packet p=this.connection.on_sendable();	
-		ByteBuffer buf=ByteBuffer.wrap(p.data);	
-		this.channel.write(buf);
+		ByteBuffer buf=ByteBuffer.wrap(p.to_bytes());	
+		
+		this.channel.send(buf, new InetSocketAddress(this.ip, this.port));
 	}
 	
 
-	private void DoCommand(byte channel, String msg)
+	private void DoCommand(byte channel, String msg) throws NoSuchAlgorithmException, NothingToSend, IOException
 	{
 		if(channel==0)
 		{
+			byte[] pwd;
+			pwd = this.password.getBytes("UTF-8");
+			byte[] msg2;
+			msg2=msg.getBytes("UTF-8");
+
+			// Lol, why can't Java just concat two arrays?
+			byte[] response = new byte[pwd.length + msg2.length];
+			System.arraycopy(pwd, 0, response, 0, pwd.length);
+			System.arraycopy(msg2, 0, response, pwd.length, msg2.length);
 			
+			// Compute SHA-1
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.update(response);
+			
+			Formatter formatter = new Formatter();
+			for (byte b : md.digest()) formatter.format("%02x", b);
+			
+			response = formatter.toString().getBytes("UTF-8");
+			formatter.close();
+				
+			Send((byte)0, new String(response, "UTF-8"));
 		}
 	}
 	
-	private void AuthorizeConnection() throws NothingToSend, IOException, PacketMalformedError, NothingToRead
+	private void AuthorizeConnection() throws NothingToSend, IOException, PacketMalformedError, NothingToRead, NoSuchAlgorithmException
 	{
 		Send((byte) 0, login);
 		Recive(); //for nonce
