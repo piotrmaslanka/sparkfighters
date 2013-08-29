5271018a683aa176668bc8417252218dcb26b80d
henrietta <piotr.maslanka@henrietta.com.pl>
2013-08-29 21:57:29 +0200

    MapData fixed

--- shared/com/sparkfighters/shared/loader/jsonobjs/MapData.java
+++ shared/com/sparkfighters/shared/loader/jsonobjs/MapData.java
@@ -8,11 +8,16 @@ import com.sparkfighters.shared.physics.objects.Rectangle;
 
 public class MapData 
 {
+	public class SpawnPoint {
+		public int team_id;
+		public Vector position;
+	}
+	
 	public int id;
 	public String name;
 	public Rectangle mapSize=new Rectangle();
-	public ArrayList<Vector> spawnPoints=new ArrayList<Vector>();
+	public ArrayList<SpawnPoint> spawnPoints=new ArrayList<>();
 	public Vector sparkStart=new Vector();
 	public ArrayList<HorizSegment> platforms=new ArrayList<HorizSegment>();
-	public ArrayList<Rectangle> obstacles=new  ArrayList<Rectangle>();
+	public ArrayList<Rectangle> obstacles=new ArrayList<Rectangle>();
 }
